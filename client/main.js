c6a5a5b130a280eafd697a622ddbf6e0a93338de
itrascastro <itrascastro@gmail.com>
2016-02-10 16:09:51 +0100

    First commit

---
+++ client/main.js
@@ -0,0 +1,20 @@
+Template.main.helpers({
+  //add you helpers here
+});
+
+Template.main.events({
+  //add your events here
+});
+
+Template.main.onCreated(function () {
+  //add your statement here
+});
+
+Template.main.onRendered(function () {
+  //add your statement here
+});
+
+Template.main.onDestroyed(function () {
+  //add your statement here
+});
+
