d3d0eda1d1f1a1497fe4e29f7b0be0239813e211
itrascastro <itrascastro@gmail.com>
2016-02-11 19:14:16 +0100

    Removed `autopublish` and set up a basic publication

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,35 +1,5 @@
-var postsData = [
-  {
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-  }
-];
-
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
-
-Template.postsList.events({
-  //add your events here
-});
-
-Template.postsList.onCreated(function () {
-  //add your statement here
-});
-
-Template.postsList.onRendered(function () {
-  //add your statement here
-});
-
-Template.postsList.onDestroyed(function () {
-  //add your statement here
-});
-
